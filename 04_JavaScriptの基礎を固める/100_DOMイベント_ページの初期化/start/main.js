5b2958b578791174463095f341fa2793d1818958
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-09-09 16:02:47 +0900

    done

--- 04_JavaScriptの基礎を固める/100_DOMイベント_ページの初期化/start/main.js
+++ 04_JavaScriptの基礎を固める/100_DOMイベント_ページの初期化/start/main.js
@@ -1,10 +1,13 @@
-const dcl = document.querySelector('.dcl');
-const load = document.querySelector('.load');
+
+
+// const dcl = document.querySelector('.dcl');
+// const load = document.querySelector('.load');
 
 document.addEventListener("DOMContentLoaded", function () {
-    dcl.classList.add('done');
+    const h1 = document.querySelector('h1');
+    h1.style.color = 'red';
 });
 
-window.addEventListener("load", function () {
-    load.classList.add('done');
-});
+// window.addEventListener("load", function () {
+//     load.classList.add('done');
+// });
